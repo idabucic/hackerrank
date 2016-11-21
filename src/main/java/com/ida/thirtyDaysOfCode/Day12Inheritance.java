4be039c8dd655576a09cdc031885ddc682478172
Ida_Bucic <ida.kukovec@gmail.com>
2016-11-21 13:04:07 +0100

    new

--- src/main/java/com/ida/thirtyDaysOfCode/Day12Inheritance.java
+++ src/main/java/com/ida/thirtyDaysOfCode/Day12Inheritance.java
@@ -2,30 +2,55 @@ package com.ida.thirtyDaysOfCode;
 
 import java.util.*;
 
-class Person {
+class PersonDay12 {
 	protected String firstName;
 	protected String lastName;
 	protected int idNumber;
-	
+
 	// Constructor
-	Person(String firstName, String lastName, int identification){
+	PersonDay12(String firstName, String lastName, int identification) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = identification;
 	}
-	
+
 	// Print person data
-	public void printPerson(){
-		 System.out.println(
-				"Name: " + lastName + ", " + firstName 
-			+ 	"\nID: " + idNumber); 
+	public void printPerson() {
+		System.out.println("Name: " + lastName + ", " + firstName + "\nID: " + idNumber);
 	}
-	 
+
 }
 
-class Student extends Person{
+class Student extends PersonDay12 {
 	private int[] testScores;
-   
+
+	Student(String firstName, String lastName, int id, int[] scores) {
+		super(firstName, lastName, id);
+		this.testScores = scores;
+	}
+
+	public char calculate() {
+		int sum = 0;
+		char res = 'T';
+		for (int i = 0; i < testScores.length; i++) {
+			sum += testScores[i];
+		}
+		int avg = (Integer) (sum / testScores.length);
+
+		if (avg >= 90 && avg <= 100) {
+			res = 'O';
+		} else if (avg >= 80 && avg < 90) {
+			res = 'E';
+		} else if (avg >= 70 && avg < 80) {
+			res = 'A';
+		} else if (avg >= 55 && avg < 70) {
+			res = 'P';
+		} else if (avg >= 40 && avg < 55) {
+			res = 'D';
+		}
+		return res;
+	}
+
 }
 
 public class Day12Inheritance {
@@ -37,14 +62,14 @@ public class Day12Inheritance {
 		int id = scan.nextInt();
 		int numScores = scan.nextInt();
 		int[] testScores = new int[numScores];
-		for(int i = 0; i < numScores; i++){
+		for (int i = 0; i < numScores; i++) {
 			testScores[i] = scan.nextInt();
 		}
 		scan.close();
-		
+
 		Student s = new Student(firstName, lastName, id, testScores);
 		s.printPerson();
-		System.out.println("Grade: " + s.calculate() );
+		System.out.println("Grade: " + s.calculate());
 
 	}
 
--- src/main/java/com/ida/thirtyDaysOfCode/Day4ClassVsInstance.java
+++ src/main/java/com/ida/thirtyDaysOfCode/Day4ClassVsInstance.java
@@ -2,6 +2,45 @@ package com.ida.thirtyDaysOfCode;
 
 import java.util.Scanner;
 
+class PersonDay4 {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public PersonDay4(int initialAge) {
+		// Add some more code to run some checks on initialAge
+		if (initialAge < 0) {
+			this.age = 0;
+			System.out.println("Age is not valid, setting age to 0.");
+		} else {
+			this.age = initialAge;
+		}
+	}
+
+	public void amIOld() {
+		// Write code determining if this person's age is old and print the
+		// correct statement:
+		if (age < 13) {
+			System.out.println("You are young.");
+		} else if (age >= 13 && age < 18) {
+			System.out.println("You are a teenager.");
+		} else {
+			System.out.println("You are old.");
+		}
+	}
+
+	public void yearPasses() {
+		// Increment this person's age.
+		this.age++;
+	}
+
+}
 //https://www.hackerrank.com/challenges/30-class-vs-instance
 public class Day4ClassVsInstance {
 	public static void main(String[] args) {
@@ -9,7 +48,7 @@ public class Day4ClassVsInstance {
 		int T = sc.nextInt();
 		for (int i = 0; i < T; i++) {
 			int age = sc.nextInt();
-			Person p = new Person(age);
+			PersonDay4 p = new PersonDay4(age);
 			p.amIOld();
 			for (int j = 0; j < 3; j++) {
 				p.yearPasses();
